352096002e58f63b36d190b8903471c32ae2f284
skl-814 <skl2007814@163.com>
2022-07-04 19:14:07 +0800

    123

--- biliget.py
+++ biliget.py
@@ -1,8 +1,41 @@
 import requests as res
 from os import system as cmd
-from sys import exit
+import os
+import sys
+import wget
+from colorama import Fore as cfore
 
-print(f"Use the ten api(https://tenapi.cn/)\nProgram version:1.2")
+__version__ = '1.3'
+__doc__ = """
+
+An tool to download videos form website like bilibili,it depend on tenapi
+(https://tenapi.cn).
+
+"""
+def cmddl():
+    cmdinli = sys.argv[1:]
+    dict = {}
+    if len(cmdinli) != 0:
+
+        if 'https://' == cmdinli[0][0:8] or 'http://' == cmdinli[0][0:7]:
+            dict['url'] = cmdinli[0]
+            if '-h' or '--help' in cmdinli:
+                print(__version__+__doc__)
+                a = 0
+        for t in cmdinli:
+            a += 1
+            if t == '-o':
+                try:
+                    os.listdir(t)
+                except:
+                    print(cfore.YELLOW+ "Could not list the output dir" +cfore.RESET)
+                else:
+                    dict['outdir'] = cmdinli[a]
+
+
+
+cmddl()
+print(f"Use the ten api(https://tenapi.cn/)\nProgram version:1.3")
 u = input("Video url:\n")
 apiurl1 = 'https://tenapi.cn/video/?url=' + u
 
@@ -23,7 +56,7 @@ elif p in ["A","a"]:
         if i in ["Y","y"]:
             cmd(f"wget.exe \"{vurl}\" ")
         elif i in ["N","n"]:
-            exit()
+            sys.exit()
         else:
             print("Wrong!")
-            exit()
+            sys.exit()
